4e50ee783b58863f9f389ecf899486927be73e1c
ronak1310 <ronaksharma80800@gmail.com>
2026-02-03 10:11:41 +0000

    day2

---
+++ DS_PYTHON/Day2/setss.py
@@ -0,0 +1,5 @@
+st={1,2,3,4}
+st.add(5)
+st.pop()
+st.remove(2)
+print(st)
--- DS_PYTHON/Day2/tsk3.py
+++ DS_PYTHON/Day2/tsk3.py
@@ -1,12 +1,24 @@
+# lst=[2,3,4,4,4,5,2,1,3,4,4,6,7,2,4,1,4,2,3,3]
+# x=4
+# i=0
+# while(i<len(lst)-1):
+#     j=i+1
+#     if(lst[i]==lst[j] and lst[i]==x):
+#         lst.pop(j)
+#         lst.pop(i)
+#     elif(lst[i]==x):
+#         lst.pop(i)
+#     else:
+#         i+=1
+# print(lst)
+
 lst=[2,3,4,4,4,5,2,1,3,4,4,6,7,2,4,1,4,2,3,3]
-temp=[]
-x=4
-i=1
-for i in range(len(lst)):
-    if lst[i]==lst[i-1]==x:
-        temp.append(i-1)
-j=0
-for j in range(len(temp)):
-    lst.remove(lst[temp[j]])
-    
-print(lst)
+i=0
+while i<len(lst)-1:
+    if(lst[i]==4 and lst[i+1]==4):
+        while lst[i]==4 and lst[i+1]==4:
+            lst.pop(i)
+        lst.pop(i)
+    else:
+        i+=1
+print(lst) 
